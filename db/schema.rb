5dbc750a3944456154b6c1d733e3c381aec3db64
Sam <30859143+samoneilll@users.noreply.github.com>
2019-04-24 16:50:56 +0200

    adding monetary_accounts

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_04_24_113406) do
+ActiveRecord::Schema.define(version: 2019_04_24_142541) do
+
+  create_table "accounts", force: :cascade do |t|
+    t.integer "bunq_id"
+    t.integer "user_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "email"
